420eb64be9c01ee14c0be42f2fdafbaa04c4e941
Aurel Medvegy <aurel.medvegy@ctoolkit.org>
2017-11-18 06:02:56 +0100

    The bidirectional Google API client DateTime vs. standard java Date converter has been added.

---
+++ adapter/src/main/java/org/ctoolkit/restapi/client/adapter/DateTimeToDateConverter.java
@@ -0,0 +1,45 @@
+package org.ctoolkit.restapi.client.adapter;
+
+import com.google.api.client.util.DateTime;
+import ma.glasnost.orika.MappingContext;
+import ma.glasnost.orika.converter.BidirectionalConverter;
+import ma.glasnost.orika.metadata.Type;
+
+import java.util.Date;
+
+/**
+ * The bidirectional Google API client {@link DateTime} vs. standard java {@link Date} converter.
+ * If needed configure your orika mapper:
+ * <pre>
+ * {@code
+ *
+ * import ma.glasnost.orika.converter.ConverterFactory;
+ * import ma.glasnost.orika.MapperFactory;
+ * ..
+ *
+ *   @literal @Inject
+ *    MapperFactory factory;
+ *
+ *    ..
+ *    ConverterFactory converterFactory = factory.getConverterFactory();
+ *    converterFactory.registerConverter( new DateTimeToDateConverter() );
+ * }
+ * </pre>
+ *
+ * @author <a href="mailto:aurel.medvegy@ctoolkit.org">Aurel Medvegy</a>
+ */
+public class DateTimeToDateConverter
+        extends BidirectionalConverter<DateTime, Date>
+{
+    @Override
+    public Date convertTo( DateTime source, Type<Date> destinationType, MappingContext context )
+    {
+        return new Date( source.getValue() );
+    }
+
+    @Override
+    public DateTime convertFrom( Date source, Type<DateTime> destinationType, MappingContext context )
+    {
+        return new DateTime( source );
+    }
+}
